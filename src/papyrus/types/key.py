d931457545bd48bfcf6de4cb65b73808ae3eb7fe
cmj <cmj@cmj.tw>
2023-06-19 00:12:29 +0800

    feat(PoC|type): refine the Key type

--- src/papyrus/types/key.py
+++ src/papyrus/types/key.py
@@ -2,178 +2,19 @@
 from __future__ import annotations
 
 import enum
-import os
-import random
-import time
 from typing import Any
-from typing import Generator
 
 from ._base import Deserializable
 from ._base import Serializable
 
 
-class CrockfordBase32:
-    """the Crockford's Base32 algo"""
-    ENCODING_TABLE = "0123456789ABCDEFGHJKMNPQRSTVWXYZ"
-
-    @staticmethod
-    def encode(key: int, size: int | None = None) -> str:
-        """encode the key as Crockford's Base32 string."""
-
-        chars = list(CrockfordBase32._encode(key))
-        chars = chars[::-1]
-
-        if size and len(chars) < size:
-            padding = CrockfordBase32.ENCODING_TABLE[0] * (size - len(chars))
-            chars.append(padding)
-
-        return "".join(chars)
-
-    @staticmethod
-    def _encode(key: int) -> Generator[str, None, None]:
-        while key > 0:
-            yield CrockfordBase32.ENCODING_TABLE[key & 0x1F]
-            key >>= 5
-
-        return
-
-
-class UniqueID(Serializable, Deserializable):
-    """
-    The UniqueID is the 128-bit unsigned integer.
-
-    It is similar to the ULID but contains the process id and cluster id.
-
-    The first 48 bits are the timestamp in milliseconds since the Unix epoch,
-    then the following 8 bits are the process id, the next 8 bits are the
-    cluster id, and the last 64 bits are random bits.
-
-    +---------------------------------------------------------------+
-    |                      32_bit_uint_time_high                    |
-    +---------------------------------------------------------------+
-    |     16_bit_uint_time_low      |   8 bit pid   |   8 bit cid   |
-    +---------------------------------------------------------------+
-    |                       32_bit_uint_random                      |
-    +---------------------------------------------------------------+
-    |                       32_bit_uint_random                      |
-    +---------------------------------------------------------------+
-
-    In this implementation, the primary key can generate 2^64 unique keys
-    per millisecond per cluster per process.
-    """
-    MIN = 0x00000000000000000000000000000000
-    MAX = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
-
-    LENGTH = 26
-
-    def __init__(self, key: int):
-        self._validate(key)
-        self._key = key
-
-    def __hash__(self):
-        return hash(self._key)
-
-    def __eq__(self, other):
-        match other:
-            case UniqueID():
-                return self._key == other.key
-            case _:
-                return self._key == other
-
-    def __lt__(self, other):
-        match other:
-            case UniqueID():
-                return self.key < other.key
-            case _:
-                return self.key < other
-
-    def __repr__(self):
-        """show the key with verbose."""
-        return f"#{str(self)} {self.timestamp=} {self.cluster_id=} {self.process_id=}"
-
-    def __str__(self):
-        """show the key as Crockford's Base32."""
-        return CrockfordBase32.encode(self._key, size=self.LENGTH)
-
-    @staticmethod
-    def _validate(key: int):
-        """validate the key is a 128-bit unsigned integer."""
-        if not isinstance(key, int):
-            raise TypeError(f"The key must be an integer: {key}")
-
-        if key & 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF == key:
-            return
-
-        raise ValueError(f"The key must be a 128-bit unsigned integer: {key}")
-
-    @property
-    def key(self) -> int:
-        return self._key
-
-    @staticmethod
-    def new(
-            timestamp: int | None = None,
-            cluster_id: int | None = None,
-            process_id: int | None = None,
-            randomness: int | None = None,
-    ) -> UniqueID:
-        """
-        create a new UniqueID by timestamp and cluster_id
-        """
-        timestamp = timestamp if timestamp is not None else int(time.time() * 1000)
-        cluster_id = cluster_id if cluster_id is not None else random.randint(0, 255)
-        process_id = process_id if process_id is not None else os.getpid() % 256
-        randomness = randomness if randomness is not None else random.randint(0, 0xFFFFFFFFFFFFFFFF)
-
-        if not isinstance(timestamp, int) or not 0 <= timestamp < (1 << 64):
-            raise ValueError(f"timestamp must be an integer between 0 ~ 1<<64: {timestamp}")
-
-        if not isinstance(cluster_id, int) or not 0 <= cluster_id < (1 << 8):
-            raise ValueError(f"cluster_id must be an integer between 0 ~ 1<<8: {cluster_id}")
-
-        if not isinstance(process_id, int) or not 0 <= process_id < (1 << 8):
-            raise ValueError(f"process_id must be an integer between 0 ~ 1<<8: {process_id}")
-
-        if not isinstance(randomness, int) or not 0 <= randomness < (1 << 64):
-            raise ValueError(f"randomness must be an integer between 0 ~ 1<<64: {randomness}")
-
-        key = (timestamp << 80) | (process_id << 72) | (cluster_id << 64) | randomness
-        return UniqueID(key)
-
-    @property
-    def timestamp(self) -> int:
-        """get the timestamp in milliseconds since the Unix epoch."""
-        return self._key >> 80
-
-    @property
-    def cluster_id(self) -> int:
-        """get the cluster id."""
-        return (self._key >> 64) & 0xFF
-
-    @property
-    def process_id(self) -> int:
-        """get the process id."""
-        return (self._key >> 72) & 0xFF
-
-    def to_bytes(self) -> bytes:
-        """convert the key to bytes."""
-        byteorder = "big"
-        return self._key.to_bytes(16, byteorder=byteorder)
-
-    @classmethod
-    def from_bytes(cls, data: bytes) -> UniqueID:
-        """convert the bytes to the key."""
-        byteorder = "big"
-        raw = int.from_bytes(data, byteorder=byteorder)
-        return cls(raw)
-
-
 class KeyType(enum.Enum):
     BOOL = 0    # the truth value, 1 byte
     WORD = 1    # the signed integer, 2 byte
-    INT = 2     # the signed integer, 16 byte
-    STR = 3     # the 63-bytes null-end string
-    TEXT = 4    # the 255-bytes arbitrary string
+    INT = 2     # the signed integer, 8 byte
+    UID = 3     # the unsigned integer, 16 byte
+    STR = 4     # the 63-bytes null-end string
+    TEXT = 5    # the 255-bytes arbitrary string
 
     def __repr__(self):
         return self.name
@@ -195,6 +36,8 @@ class KeyType(enum.Enum):
             case KeyType.WORD:
                 return 2
             case KeyType.INT:
+                return 8
+            case KeyType.UID:
                 return 16
             case KeyType.STR:
                 return 64
@@ -210,14 +53,28 @@ class KeyType(enum.Enum):
                 return KeyType.BOOL
             case int() if -32768 <= raw <= 32767:
                 return KeyType.WORD
-            case int():
+            case int() if -9223372036854775808 <= raw <= 9223372036854775807:
                 return KeyType.INT
+            case int() if 0 <= raw <= 340282366920938463463374607431768211455:
+                return KeyType.UID
             case str() if len(raw) < 64:
                 return KeyType.STR
             case str() if len(raw) < 256:
                 return KeyType.TEXT
             case _:
                 raise NotImplementedError(f"unknown key type: {raw} ({type(raw)}=)")
+
+    def cast(self, raw: Any) -> Any:
+        match self:
+            case KeyType.BOOL:
+                value = bool(raw)
+            case KeyType.WORD | KeyType.INT | KeyType.UID:
+                value = int(raw)
+            case KeyType.STR | KeyType.TEXT:
+                value = str(raw)
+            case _:
+                raise NotImplementedError(f"unknown key type: {self}")
+        return value
 
 
 class Key(Serializable, Deserializable):
@@ -229,14 +86,15 @@ class Key(Serializable, Deserializable):
     and convert from the binary format to the key.
     """
     def __init__(self, raw: Any, ktype: KeyType | None = None):
-        self._raw = raw
         self._ktype = KeyType.detect(raw) if ktype is None else ktype
-        self._value = None
+        self._value = self._ktype.cast(raw)
 
     def __repr__(self):
+        """show the human-readable representation of the key."""
         return f"<Key #{self.ktype} {self.value}>"
 
     def __str__(self):
+        """show the raw value as string."""
         return str(self.value)
 
     def __eq__(self, other):
@@ -257,10 +115,6 @@ class Key(Serializable, Deserializable):
 
     def __hash__(self):
         return hash(self.value)
-
-    @property
-    def raw(self) -> Any:
-        return self._raw
 
     @property
     def ktype(self) -> KeyType:
@@ -285,7 +139,7 @@ class Key(Serializable, Deserializable):
         match ktype:
             case KeyType.BOOL:
                 return Key(bool(self.value), ktype)
-            case KeyType.WORD | KeyType.INT:
+            case KeyType.WORD | KeyType.INT | KeyType.UID:
                 return Key(int(self.value), ktype)
             case KeyType.STR | KeyType.TEXT if self.ktype == KeyType.BOOL:
                 return Key(str(int(self.value)), ktype)
@@ -294,11 +148,12 @@ class Key(Serializable, Deserializable):
             case _:
                 raise NotImplementedError(f"unknown key type: {ktype}")
 
+    # ======== Serializable ========
     def to_bytes(self) -> bytes:
         match self.ktype:
             case KeyType.BOOL:
                 return int(self.value).to_bytes(self.ktype.cap(), byteorder="big", signed=True)
-            case KeyType.WORD | KeyType.INT:
+            case KeyType.WORD | KeyType.INT | KeyType.UID:
                 return self.value.to_bytes(self.ktype.cap(), byteorder="big", signed=True)
             case KeyType.STR | KeyType.TEXT:
                 data = self.value.encode("utf-8")
@@ -307,6 +162,7 @@ class Key(Serializable, Deserializable):
             case _:
                 raise NotImplementedError(f"unknown key type: {self.ktype}")
 
+    # ======== Deserializable ========
     @classmethod
     def from_bytes(cls, data: bytes) -> Key:
         match len(data):
@@ -316,9 +172,12 @@ class Key(Serializable, Deserializable):
             case 2:
                 raw = int.from_bytes(data, byteorder="big", signed=True)
                 ktype = KeyType.WORD
-            case 16:
+            case 8:
                 raw = int.from_bytes(data, byteorder="big", signed=True)
                 ktype = KeyType.INT
+            case 16:
+                raw = int.from_bytes(data, byteorder="big", signed=True)
+                ktype = KeyType.UID
             case 64:
                 raw = data.decode("utf-8").rstrip("\x00")
                 ktype = KeyType.STR
