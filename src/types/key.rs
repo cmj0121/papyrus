4842d05024e801d1956b978819a6fda7130f9721
cmj <cmj@cmj.tw>
2023-07-01 10:46:46 +0800

    feat(types): define the Key and Value type
        Papyrus provides two data type: Key and Value.
    
        Key is the searchable and sortable data type with fixed length and
        pre-define type. It is the primary key or index of key-value pair in
        Papyrus.
    
        Value is the arbitrary length data in Papyrus uploaded from user.

---
+++ src/types/key.rs
@@ -0,0 +1,8 @@
+//! Key is the searchable and sortable data type used in Papyrus.
+
+/// Key is the searchable and sortable data type used in Papyrus.
+///
+/// It is the fixed length and well-known type that is used as the primary
+/// key or index for the key-value pairs in Papyrus.
+#[derive(Debug, Clone, PartialEq, PartialOrd, Hash)]
+pub enum Key {}
---
+++ src/types/value.rs
@@ -0,0 +1,9 @@
+//! Value is the arbitrary length data used in Papyrus.
+
+/// Value is the arbitrary length data used in Papyrus.
+///
+/// It is the arbitrary length data upload from user and stored in the
+/// Papyrus. It can be any binary data and may compressed or store as
+/// another detached file.
+#[derive(Debug, Clone)]
+pub enum Value {}
