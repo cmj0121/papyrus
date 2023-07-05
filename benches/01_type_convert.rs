29ec611204abcd5b03265662a929349c45c3c2cb
cmj <cmj@cmj.tw>
2023-07-05 10:12:32 +0800

    test(bench): add the benchmark for type packer

--- benches/01_type_convert.rs
+++ benches/01_type_convert.rs
@@ -1,5 +1,5 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use papyrus::{Converter, Key, Value};
+use papyrus::{Converter, Key, Packer, Value};
 
 pub fn key_convert(c: &mut Criterion) {
     let int: i64 = 34182;
@@ -7,6 +7,9 @@ pub fn key_convert(c: &mut Criterion) {
 
     let int_bytes = Key::from(int).to_bytes();
     let chr_bytes = Key::from(chr).to_bytes();
+
+    let int_packed = Key::from(int).pack();
+    let chr_packed = Key::from(chr).pack();
 
     c.bench_function("int to Key", |b| b.iter(|| Key::from(int)));
     c.bench_function("chr to Key", |b| b.iter(|| Key::from(chr)));
@@ -24,11 +27,18 @@ pub fn key_convert(c: &mut Criterion) {
     c.bench_function("chr to Key Converter (from_bytes)", |b| {
         b.iter(|| Key::from_bytes(&chr_bytes))
     });
+
+    c.bench_function("int to Key pack", |b| b.iter(|| Key::from(int).pack()));
+    c.bench_function("chr to Key pack", |b| b.iter(|| Key::from(chr).pack()));
+
+    c.bench_function("int to Key unpack", |b| b.iter(|| Key::unpack(&int_packed)));
+    c.bench_function("chr to Key unpack", |b| b.iter(|| Key::unpack(&chr_packed)));
 }
 
 pub fn value_convert(c: &mut Criterion) {
     let text: &str = "Test 測試 テスト prüfen ทดสอบ";
     let text_bytes = Value::from(text).to_bytes();
+    let text_packed = Value::from(text).pack();
 
     c.bench_function("str to Value", |b| b.iter(|| Value::from(text)));
 
@@ -38,6 +48,12 @@ pub fn value_convert(c: &mut Criterion) {
 
     c.bench_function("str to Value Converter (from_bytes)", |b| {
         b.iter(|| Value::from_bytes(&text_bytes))
+    });
+
+    c.bench_function("str to Value pack", |b| b.iter(|| Value::from(text).pack()));
+
+    c.bench_function("str to Value unpack", |b| {
+        b.iter(|| Value::unpack(&text_packed))
     });
 }
 
