3d7405c66d26f2dac8631f24ab326e1d0aab5a8c
cmj <cmj@cmj.tw>
2023-07-12 09:22:01 +0800

    feat(revdb|cli): setup logger sub-system

--- revdb/src/cli.rs
+++ revdb/src/cli.rs
@@ -1,5 +1,6 @@
 //! The command-line tool for RevDB.
 use clap::Parser as ClapParser;
+use tracing::trace;
 
 /// The command-line tool for RevDB.
 #[derive(Debug, ClapParser)]
@@ -20,6 +21,40 @@ impl RevDB {
 
     /// execute revdb with the given arguments, and return the exit code.
     fn run(&self) -> i32 {
+        self.setup_logging();
+
+        self.prologue();
+        // what you want to do here ...
+        self.epologue();
+
         0
+    }
+
+    /// setup the logging system
+    fn setup_logging(&self) {
+        let subscriber = tracing_subscriber::FmtSubscriber::builder()
+            .with_max_level(match self.verbose.log_level() {
+                Some(level) => match level {
+                    clap_verbosity_flag::Level::Error => tracing::Level::ERROR,
+                    clap_verbosity_flag::Level::Warn => tracing::Level::WARN,
+                    clap_verbosity_flag::Level::Info => tracing::Level::INFO,
+                    clap_verbosity_flag::Level::Debug => tracing::Level::DEBUG,
+                    _ => tracing::Level::TRACE,
+                },
+                None => tracing::Level::ERROR,
+            })
+            .finish();
+
+        tracing::subscriber::set_global_default(subscriber).expect("failed to set logger");
+    }
+
+    /// setup everything before running
+    fn prologue(&self) {
+        trace!("prologue ...");
+    }
+
+    /// clean-up everything after running
+    fn epologue(&self) {
+        trace!("epologue ...");
     }
 }
