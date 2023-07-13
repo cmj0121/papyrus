bae55478cfffc5d8b4fa0551d1db5e259bab3557
cmj <cmj@cmj.tw>
2023-07-13 09:05:11 +0800

    feat(revdb|settings): add the base storage folder

--- src/revdb/src/settings.rs
+++ src/revdb/src/settings.rs
@@ -6,10 +6,26 @@ use serde::{Deserialize, Serialize};
 pub struct Settings {
     /// to enable the debug information
     pub debug: bool,
+
+    /// the base directory for the storage
+    pub base: String,
+}
+
+impl Settings {
+    /// Get the default base directory.
+    fn default_basedir() -> String {
+        match home::home_dir() {
+            Some(path) => path.join(".revdb").to_str().unwrap().to_string(),
+            None => ".revdb".to_string(),
+        }
+    }
 }
 
 impl Default for Settings {
     fn default() -> Self {
-        Self { debug: false }
+        Self {
+            debug: false,
+            base: Self::default_basedir(),
+        }
     }
 }
